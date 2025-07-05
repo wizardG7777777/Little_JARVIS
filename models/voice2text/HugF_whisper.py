5dbf511ea85dfbd89cc97b8a54142904b5d36491
wizzardG7777777 <wizzardsckor777@gmail.com>
2025-07-06 02:33:51 +0800

    Module upgrade, more flexible

--- models/voice2text/HugF_whisper.py
+++ models/voice2text/HugF_whisper.py
@@ -1,5 +1,7 @@
 import os
 import glob
+
+import numpy
 import torch
 import soundfile as sf
 import time # for stage1 timing test, can be deleted when stage1 test is done
@@ -14,13 +16,36 @@ def __folder_path_extract(input_path:str)->list:
     else:
         print("Invalid input path")
         return [] # return empty list if input is not a valid directory, and make sure the function will not break the program
+def audio_reading (sample_rate:int, file_path:str="../../TestMaterial/human_speech/output/")-> list:
+    # Read audio file and return audio data and sample rate
+    # If no input parameter is used, use default path
+    audio_array = [sf.read(audio) for audio in __folder_path_extract(input_path=file_path)]
+    audio_data = []
+    for audio in audio_array:
+        if audio[1] == sample_rate:
+            audio_data.append(audio)
+        else:
+            # print(f"Audio {audio} is not 16000Hz, please resample it")
+            pass
+    return audio_data
+def audio_from_mic() -> list:
+    # This function set up for future, ready for reading from mic, it suppose to return a list which contains audio data
 
-def audio_transcription(input_path:str, output_path:str="./")->str:
+   """
+   Explanation:
+   for every element in list, they are:
+   first element is audio data, which is numpy array
+   second element is sample rate, which is int, and should be 16000 by default
+   """
+   return []
+def audio_transcription(input_audio_data:list,
+                        voice_model:str="./whisper-base",
+                        output_path:str="./")->str:
     # Use cuda priority, then metal, last cpu
     device = "cuda" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu")
 
     # load model and processor in same directory
-    model_name = "./whisper-large-v3-turbo"
+    model_name = voice_model
     audio_processor = WhisperProcessor.from_pretrained(model_name)
     audio_model = WhisperForConditionalGeneration.from_pretrained(model_name).to(device)
 
@@ -30,13 +55,11 @@ def audio_transcription(input_path:str, output_path:str="./")->str:
     tuple[1] is the sample rate, which is int, and should be 16000, other wise it will lead error
     """
 
-    audio_array = [sf.read(audio) for audio in __folder_path_extract(input_path=input_path)]
     sample_rate = 16000
-    # load all audio data with 16000 sample rate, terminate loading process when audio array is empty
-    if not audio_array:
-        raise RuntimeError("No audio data is loaded, please check your audio path and sample rate, also be patient with debugging")
-    else:
-        audio_data = [a[0] for a in audio_array if a[1] == sample_rate]
+    audio_data = [a[0] for a in input_audio_data if a[1] == sample_rate]
+    if len(audio_data) == 0:
+        print("No audio data, please check the input audio data")
+        return ""
 
     # feature extract and pre-process
     inputs = audio_processor(
@@ -69,5 +92,6 @@ def audio_transcription(input_path:str, output_path:str="./")->str:
     return result
 
 if __name__ == "__main__":
-    ans = audio_transcription("../../TestMaterial/human_speech/output/")
+    test_audio_data = audio_reading(16000, "../../TestMaterial/human_speech/output/")
+    ans = audio_transcription(input_audio_data=test_audio_data)
     print(ans)
